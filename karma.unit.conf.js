7a141c0a604918c5a354368674e4063f452ea455
chirgwin <geoff@portchaw.com>
2016-04-04 00:06:51 -0700

    remove bower in favor of all-npm

--- karma.unit.conf.js
+++ karma.unit.conf.js
@@ -8,9 +8,9 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
 
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-jsbn-big-integer/big-integer.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-jsbn-big-integer/big-integer.js',
       'src/snowflake.js',
       'test/*.spec.js'
     ],
